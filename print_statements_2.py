b2c5d3e99a2247fba6d8d84ca0b4ad4720182a63
niharikanatarajan2-code <niharikanatarajan2@gmail.com>
2025-12-30 17:39:56 +0000

    Add files via upload

---
+++ print_statements_2.py
@@ -0,0 +1,10 @@
+#Print a Sentance
+print("Welcome to the world of programming")
+#Print a Number
+print(4)
+#Next Line after print
+print("Hello python \n")
+#Pass multiple arguments for printing 
+print("hello", 5)
+#End argument of Print Statement
+print("Welcome to", end="*")
