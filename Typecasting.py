cc62d85ef6388c8baf56f46dbf8ab8dc7dc67da3
niharikanatarajan2-code <niharikanatarajan2@gmail.com>
2026-01-05 17:08:36 +0000

    Add files via upload

---
+++ Typecasting.py
@@ -0,0 +1,18 @@
+name="Penguin"
+age=15
+is_student=True
+weight=38.5
+print("Name:" ,name)
+print("Data Type of name is" ,type(name))
+print("Age:" ,age)
+print("Data Type of age is" , type(age))
+print("is_student:",is_student)
+print("Data Type of is_student is" ,type(is_student))
+print("Weight:" ,weight)
+print("Data Type of weight is" ,type(weight))
+age=str(age)
+print(age)
+print("Data Type of age is" ,type(age))
+weight=int(weight)
+print(weight)
+print("Data Type of Weight is" ,type(weight))
