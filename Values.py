64808ca8ee92b1d5b3c73119888489e3fa249a0c
niharikanatarajan2-code <niharikanatarajan2@gmail.com>
2026-02-10 17:52:25 +0000

    Add files via upload

---
+++ Values.py
@@ -0,0 +1,12 @@
+num1=10
+num2=14
+num3="hello"
+num4=8
+print("The value inside num1 is", num1)
+print("The value inside num2 is", num2)
+print("The value inside num3 is", num3)
+print("Value after swapping")
+num1=num2
+num3=num4
+print("Value in num1 after swapping is", num1)
+print("Value in num3 after swapping is", num3)
