20b25ce3f5fbb0e3c66e2c575fc7ebb348acc70a
niharikanatarajan2-code <niharikanatarajan2@gmail.com>
2026-03-18 18:09:19 +0000

    Add files via upload

--- Infinate_Loop.py
+++ Infinate_Loop.py
@@ -1,3 +1,9 @@
-i=0
-while i<=0:
-    print("I WILL RUN FOREVER")
+valid=False
+while not valid:
+    try:
+        n=int(input("Enter a number: "))
+        while n%2==0:
+            print("bye")
+            valid=True
+    except ValueError:
+            print("Invalid")
