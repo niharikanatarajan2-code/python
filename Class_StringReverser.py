930fa701b7925a6324e8b0f327468a4ffbfabe5b
niharikanatarajan2-code <niharikanatarajan2@gmail.com>
2026-04-29 19:18:17 +0100

    Add files via upload

---
+++ Class_StringReverser.py
@@ -0,0 +1,10 @@
+class StringReverser:
+    def __init__(self):
+        self.text=self.text
+        def reverse_words(self):
+            words=self.text.split()
+            reversed_words=words[::-1]
+            return ' '.join(reversed_words)
+        s=StringReverser("Hello world this is Python")
+        print(s.reverse_words())
+  
