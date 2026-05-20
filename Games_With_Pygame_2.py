c27220e8aff4a05cc29cd4ed61f08b31862b4bc5
niharikanatarajan2-code <niharikanatarajan2@gmail.com>
2026-05-20 21:07:53 +0100

    Add files via upload

---
+++ Games_With_Pygame_2.py
@@ -0,0 +1,22 @@
+import pygame
+pygame.init()
+screen = pygame.display.set_mode((500, 400))
+clock = pygame.time.Clock()
+player = pygame.Rect(50, 50, 50, 50)
+enemy = pygame.Rect(300, 200, 50, 50)
+run = True
+while run:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_LEFT]:
+        player.x -= 5
+    if keys[pygame.K_RIGHT]:
+        player.x += 5        
+    screen.fill((0, 0, 0))
+    pygame.draw.rect(screen, (255, 0, 0), player)
+    pygame.draw.rect(screen, (0, 255, 0), enemy)    
+    pygame.display.update()
+    clock.tick(60)
+pygame.quit()
