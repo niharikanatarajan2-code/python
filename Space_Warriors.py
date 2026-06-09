578dee8dbb2b5f8838c9ef11900b33c07d2ae4d3
niharikanatarajan2-code <niharikanatarajan2@gmail.com>
2026-06-09 18:42:22 +0100

    Add files via upload

--- Space_Warriors.py
+++ Space_Warriors.py
@@ -23,4 +23,42 @@ for i in range(6):
     bullet_state="ready"
     score=0
     font=pygame.font.Font("freesansbold.ttf",32)
-               
+def show_score():
+    txt=font.render("Score:"+str(score),True,(255,255,255))
+    screen.blit(txt,(10,10))
+def collision(ex,ey,bx,by):
+    return math.sqrt((ex-bx)**2+(ey-by)**2)<27
+running=True
+while running:
+    screen.blit(bg,(0,0))
+    for event in pygame.event.get():
+        if event.type==pygame.QUIT:
+            running=False
+        if event.type==pygame.KEYDOWN:
+                if event.key==pygame.K_LEFT:
+                    player_speed=-5
+                if event.key==pygame.K_RIGHT:
+                        player_speed=5
+                if event.key==pygame.K_SPACE and bullet_state=="ready":
+                    bullet_x=player_x
+                    bullet_state="fire"
+                    if event.type==pygame.KEYUP:
+                         player_speed=0
+                         player_x += player_speed
+                         player_x = max(0,min(player_x,736))
+                         screen.blit(player_img,(player_x,player_y))
+                         if bullet_state=="fire":
+                              screen.blit(bullet_img,(bullet_x+16,bullet_y))
+                              bullet_y-=10
+                              if bullet_y<=0:
+                                   bullet_y=380
+                                   bullet_state="ready"
+                                   for e in enemies:
+                                        e[0]+=e[2]
+                                        if e[0]<=0 or e[0]>=736:
+                                             e[2]*=-1
+                                             e[1]+=40
+                                             if collision(e[0],e[1],bullet_x,bullet)
+                     
+
+    
