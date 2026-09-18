d46d1ca4dd31678acc26686ed34a8d5d931bfbbe
niharikanatarajan2-code <niharikanatarajan2@gmail.com>
2026-09-18 18:57:52 +0100

    Add files via upload

---
+++ SQLite_Homework.py
@@ -0,0 +1,9 @@
+import sqlite3
+database = 'database.sqlite'
+conn = sqlite3.connect(database)
+print('Opened data successfully')
+import pandas as pd
+tables = pd.read_sql("""SELECT *
+                  FROM sqlite_master
+                  WHERE type='table';""",conn)
+tables
